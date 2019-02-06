46fd26a28d236d4078477709732fb3f4ed4507b9
Quentin Peter <qaep2@cam.ac.uk>
2019-02-06 11:50:54 +0100

    added tom file as test

---
+++ diffusion_device/tests/test_data/tom_mobility_r/sizescript2.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 10 12:27:04 2017
+
+@author: quentinpeter
+"""
+
+from diffusion_device import process_data
+from glob import glob
+
+# Where should the results be saved
+outpath = 'all'
+
+# json files location (use glob!)
+settingsfn = 'settings.json'
+metadatafn = 'stack_metadata.json'
+
+settings = glob(settingsfn)
+if len(settings) == 0:
+    raise RuntimeError(f"Can't find {settingsfn}")
+metadatas = glob(metadatafn)
+if len(metadatas) == 0:
+    raise RuntimeError(f"Can't find {metadatafn}")
+
+for sfn in settings:
+    for mfn in metadatas:
+        # Call function
+        process_data.full_fit(sfn, mfn, outpath)
